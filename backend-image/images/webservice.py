ad68ffece3238dcc5da95612c7fd2b86370256f3
Savio Costa <saviao@gmail.com>
2025-04-06 04:44:50 +0000

    Aplicacao funcionando, falta adicionar mecanismo para baixar pesos quando o app iniciar

--- backend-image/images/webservice.py
+++ backend-image/images/webservice.py
@@ -1,12 +1,18 @@
 import os
+import io
+import subprocess
 from os import path
 import tensorflow as tf
 import keras as keras
+from tensorflow.keras import layers
+from tensorflow.keras import Input
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Conv2D
 from PIL import Image
 import numpy as np
 from fastapi import FastAPI, File, UploadFile, Query, applications
 from fastapi.openapi.docs import get_swagger_ui_html
-from fastapi.responses import StreamingResponse, RedirectResponse, JSONResponse
+from fastapi.responses import StreamingResponse, RedirectResponse, JSONResponse, Response
 from fastapi.staticfiles import StaticFiles
 from fastapi.encoders import jsonable_encoder
 
@@ -35,6 +41,72 @@ if path.exists(assets_path + "/swagger-ui.css") and path.exists(assets_path + "/
 
     applications.get_swagger_ui_html = swagger_monkey_patch
 
+def bloco_residual(X, n, dim=(5, 5)):
+    """`
+    Entradas:
+        X = tensor de entrada
+        n = número de filtros
+        dim = dimensão dos filtros
+
+    Saída:
+        A2 -> tensor de saída
+    """
+
+    # Configuraçao do bloco
+    # Inclua o seu código aqui
+    A1 = layers.Conv2D(n, dim, strides=1, padding='same', activation='relu')(X)
+    Z2 = layers.Conv2D(n, dim, strides=1, padding='same', activation='linear')(A1)
+    ZX = layers.Add()([Z2, X])
+    A2 = layers.Activation('relu')(ZX)
+
+    # Retorna saída
+    return A2
+
+def build_model():
+  filter_size = (5, 5)
+  input_shape = (64, 64, 3)
+  X0 = layers.Input(shape=input_shape)
+
+  # Inclua seu código aqui
+  # Camada convolucional para ajustar número de canais para poder ser somada dentro do bloco residual
+  X1 = layers.Conv2D(128, filter_size, strides=1, padding='same', activation='relu')(X0)
+
+  # Primeiro bloco residual com 128 filtros
+  X2 = bloco_residual(X1,128)
+
+  # Camada convolucional para ajustar número de canais para poder ser somada dentro do bloco residual
+  X3 = layers.Conv2D(256, filter_size, strides=1, padding='same', activation='relu')(X2)
+
+  # Segundo bloco residual com 256 filtros
+  X4 = bloco_residual(X3,256)
+
+  # Terceiro bloco residual com 256 filtros
+  X5 = bloco_residual(X4,256)
+
+  # Camada convolucional para ajustar número de canais para poder ser somada dentro do bloco residual
+  X6 = layers.Conv2D(128, filter_size, strides=1, padding='same', activation='relu')(X5)
+
+  # Quarto bloco residual com 128 filtros
+  X7 = bloco_residual(X6,128)
+
+  # Quinto bloco residual com 128 filtros
+  X8 = bloco_residual(X7,128)
+
+  # Camada convolucional para ajustar número de canais para poder ser somada dentro do bloco residual
+  X9 = layers.Conv2D(64, filter_size, strides=1, padding='same', activation='relu')(X8)
+
+  # Sexto bloco residual com 64 filtros
+  X10 = bloco_residual(X9,64)
+
+  # Camada convolutional para acertar profundidade da imagem resultante no padrão RGB
+  Y = layers.Conv2D(3, filter_size, strides=1, padding='same', activation='linear')(X10)
+
+
+  # Criação da RNA
+  # Inclua seu código aqui
+  rna = Model(X0, Y)
+
+  return rna
 
 @app.get("/", response_class=RedirectResponse, include_in_schema=False)
 async def index():
@@ -55,5 +127,39 @@ async def trata_image(
 
     return JSONResponse(
         content = json_resposta
+    )
+
+@app.post("/build_nose",
+          tags=["Endpoints"], 
+          responses = { 
+              200 : { 
+                  "content": {"image/png" : {}}
+                    } 
+                    },
+                    response_class=Response)
+async def build_nose(
+    image_file: UploadFile = File(...)):
+    image = Image.open(image_file.file)
+    image = image.resize((64, 64))
+    inp = np.array(image) / 255
+    inp = inp.reshape((-1, 64, 64, 3))
+    
+    
+    build_nose_model = build_model()
+    build_nose_model.load_weights(os.getcwd() + "/images/model/model.weights.h5")
+    
+    prediction = build_nose_model.predict(inp).reshape((64, 64, 3))
+    pixels = (prediction * 255).astype(np.uint8)
+    img = Image.fromarray(pixels, 'RGB')
+    img.save(os.getcwd() + "/images/model/sample.png")
+    image_bytes = io.BytesIO()
+    img.save(image_bytes, format='PNG')
+    image_bytes.seek(0)
+    buffer = image_bytes.read()
+
+
+    return Response(
+        content = buffer, media_type="image/png"
+        #content = prediction.tolist(), media_type="text/json"
     )
 
--- gradio-image/app-gradio.py
+++ gradio-image/app-gradio.py
@@ -1,13 +1,16 @@
 import gradio as gr
 import requests
+from PIL import Image
+import io
 
 def envia(imagem):
-    url="http://backend-image:8081/imagens/"
+    url="http://localhost:8081/build_nose/"
     with open(imagem,'rb') as f:
-        r = requests.post(url, files={"image_file":f})
-    return r.content
+        r = requests.post(url, files={"image_file":f}, stream=True)
+        pilImage = Image.open(io.BytesIO(r.content))
+        return pilImage
 
-ui = gr.Interface(fn=envia, inputs=gr.Image(type="filepath"),outputs="text")
+ui = gr.Interface(fn=envia, inputs=gr.Image(type="filepath", width=256, height=256),outputs=gr.Image(type="pil", width=256, height=256))
 
 if __name__ == "__main__":
-    ui.launch(server_name="0.0.0.0",server_port=7860, show_api=False)
+    ui.launch(server_name="0.0.0.0",server_port=8080, show_api=False)
