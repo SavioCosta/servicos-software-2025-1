40fd76069235e4aad15c836d802aad3d10726682
Savio Costa <saviao@gmail.com>
2025-04-06 23:25:15 +0000

    Versao funcionando, pronta para testes e entrega

--- gradio-image/app-gradio.py
+++ gradio-image/app-gradio.py
@@ -10,7 +10,25 @@ def envia(imagem):
         pilImage = Image.open(io.BytesIO(r.content))
         return pilImage
 
-ui = gr.Interface(fn=envia, inputs=gr.Image(type="filepath", width=256, height=256),outputs=gr.Image(type="pil", width=256, height=256))
+#ui = gr.Interface(fn=envia, inputs=gr.Image(type="filepath", width=256, height=256),outputs=gr.Image(type="pil", width=256, height=256))
+with gr.Blocks() as ui:
+    with gr.Row() as header:
+        gr.Markdown(
+            """
+            # <span style='text-align:center'>Gerador de narizes :nose:</span>
+            ### <span style='text-align:center'>Saiu com pressa e esqueceu o nariz em casa?</span>
+            ### <span style='text-align:center'>Seus problemas acabaram!!!</span>
+            <span style='text-align:center'>Entre na cabine da esquerda e seja teletransportado para a direita com o nariz novo!</span>
+            """)
+            
+    with gr.Row() as body:
+        with gr.Column() as col1:
+            inputs = gr.Image(type="filepath", width=256, height=256)
+            btn = gr.Button(value="Gerar Nariz")
+        with gr.Column() as col2:
+            outputs = gr.Image(type="pil", width=256, height=256)
+    
+    btn.click(fn=envia, inputs=inputs,outputs=outputs)
 
 if __name__ == "__main__":
     ui.launch(server_name="0.0.0.0",server_port=8080, show_api=False)
